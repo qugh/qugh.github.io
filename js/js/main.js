9000d4de50a989cdfd2cba3422476a989109b627
qugh <74339537+qugh@users.noreply.github.com>
2021-12-04 00:00:18 +0300

    Add files via upload

--- js/js/main.js
+++ js/js/main.js
@@ -9,14 +9,21 @@ for (let i = 0; i < 100; i++) {
     data_array.positions.push(i + 1);
     data_array.prices.push(Math.floor(Math.random() * 100))
 }
+let currentPage = document.querySelector('.current_page');
+let updateCurrentPage = (value) => {
+    currentPage.innerHTML = `Current page: ${value}`;
+}
 
 localStorage.setItem('table_data', JSON.stringify(data_array));
 
 let my_data = JSON.parse(localStorage.getItem('table_data'));
 let page_buttons_selector;
 let page_selector = document.querySelector('#page_selector');
+let elemsOnPage_value = +page_selector.value;
 let likeButton = document.querySelectorAll('.like');
 let likePicture = document.querySelector('.like_img')
+let jumpInput = document.querySelector('.jump_toThePage')
+let jumpButton = document.querySelector('.jump_button')
 
 let nextButton = document.querySelector('.next_button');
 let backButton = document.querySelector('.back_button');
@@ -52,9 +59,9 @@ tabs.forEach(item => {
         tabs.forEach(item => {
 
             item.classList.remove('active')
-            item.children[1].innerHTML='Inactive tab'
+            item.children[1].innerHTML = 'Inactive tab'
         })
-        e.currentTarget.children[1].innerHTML='Active tab'
+        e.currentTarget.children[1].innerHTML = 'Active tab'
         e.currentTarget.classList.add('active');
     }
     )
@@ -71,12 +78,12 @@ let renderElements = (elemsOnPage, page_number) => {
             table_elements.insertAdjacentHTML('beforeend', ` <tr> <th>${my_data.positions[i]}</th><th>${my_data.locations[i] || 'default '}</th><th>${my_data.prices[i]} y.e.</th></tr>`)
     }
     setTimeout(() => table_elements.style.opacity = "1", 300);
+    updateCurrentPage(+page_number_value);
 }
-
 
 let page_switch = () => {
     page_buttons_selector.forEach(item => {
-        let elemsOnPage_value = +page_selector.value;
+        elemsOnPage_value = +page_selector.value;
         item.addEventListener('click', (e) => {
             page_buttons_selector.forEach(item => {
                 item.classList.remove('active')
@@ -95,27 +102,45 @@ let page_switch = () => {
 
 //render pages block on change condition
 let updatePageCounter = (elems_count, elems_on_page) => {
-
+let page_jumper = document.querySelector('.jump_block');
     let pages_count = Math.ceil(elems_count / elems_on_page);
     pages_counter.innerHTML = '';
+    if (pages_count > 9) {
+        for (let i = 0; i < 3; i++)
+            pages_counter.insertAdjacentHTML('beforeend', `<div><button class="${i == 0 ? 'active page_button' : 'page_button'}" id="page${i + 1}">${i + 1}</button></div>`)
+        console.log(pages_count, pages_count - 5)
+        pages_counter.insertAdjacentHTML('beforeend', `<span>...</span>`)
+        for (let j = pages_count - 3; j < pages_count; j++) {
+            pages_counter.insertAdjacentHTML('beforeend', `<div><button class="${j == 0 ? 'active page_button' : 'page_button'}" id="page${j + 1}">${j + 1}</button></div>`)
+        }
 
-    for (let i = 0; i < pages_count; i++)
-        pages_counter.insertAdjacentHTML('beforeend', `<div><button class="${i == 0 ? 'active page_button' : 'page_button'}" id="page${i + 1}">${i + 1}</button></div>`)
-    page_buttons_selector = document.querySelectorAll('.page_button');
-    pages_counter.style.gridTemplateColumns = `repeat(${(pages_count < 10) ? pages_count : 10},1fr)`;
-    page_switch();
+        page_buttons_selector = document.querySelectorAll('.page_button');
+        
+        pages_counter.style.gridTemplateColumns = `repeat(7,1fr)`;
+        page_switch();
+        page_jumper.style.display='block';
+    }
+    else {
+        for (let i = 0; i < pages_count; i++)
+            pages_counter.insertAdjacentHTML('beforeend', `<div><button class="${i == 0 ? 'active page_button' : 'page_button'}" id="page${i + 1}">${i + 1}</button></div>`)
+        page_buttons_selector = document.querySelectorAll('.page_button');
+        pages_counter.style.gridTemplateColumns = `repeat(${(pages_count < 10) ? pages_count : 10},1fr)`;
+        page_switch();
+        page_jumper.style.display='none';
+    }
 }
 
 
 
 
 //first render of elements
-let firstRender = () => {
+// let firstRender = () => {
 
-    for (let i = 0; i < page_selector.value; i++)
-        table_elements.insertAdjacentHTML('beforeend', ` <tr> <th>${my_data.positions[i]}</th><th>${my_data.locations[i] || 'default '}</th><th>${my_data.prices[i]} y.e.</th></tr>`)
-        
-}
+//     for (let i = 0; i < page_selector.value; i++)
+//         table_elements.insertAdjacentHTML('beforeend', ` <tr> <th>${my_data.positions[i]}</th><th>${my_data.locations[i] || 'default '}</th><th>${my_data.prices[i]} y.e.</th></tr>`)
+
+// }
+renderElements(elemsOnPage_value, page_number_value)
 // render elements provided that the number of elements on the page changes
 page_selector.addEventListener("change", (e) => {
 
@@ -123,15 +148,71 @@ page_selector.addEventListener("change", (e) => {
     table_elements.innerHTML = ''; // while(table_elements.firstChild...(remove))
     table_elements.style.opacity = "0"
 
-    for (let i = 0; i < e.currentTarget.value; i++) {
-        table_elements.insertAdjacentHTML('beforeend', ` <tr> <th>${my_data.positions[i]}</th><th>${my_data.locations[i] || 'default '}</th><th>${my_data.prices[i]} y.e.</th></tr>`)
 
-    }
+
+
+    renderElements(e.currentTarget.value, 1)
+    updateCurrentPage(page_number_value);
 
     setTimeout(() => table_elements.style.opacity = "1", 300);
     updatePageCounter(my_data.positions.length, e.currentTarget.value);
     page_buttons_selector[0].classList.add('active');
 })
+
+
+
+//first render pages block
+updatePageCounter(my_data.positions.length, page_selector.value);
+
+
+//add active class each child + render elements on pageNumber change
+
+nextButton.addEventListener('click', () => {
+
+    elemsOnPage_value = +page_selector.value;
+
+    if (+page_number_value < page_buttons_selector[page_buttons_selector.length - 1].id.slice(4)) {
+        page_buttons_selector.forEach(item =>
+            item.classList.remove('active'))
+
+        ++page_number_value;
+
+        renderElements(elemsOnPage_value, page_number_value)
+
+
+
+
+
+        page_buttons_selector.forEach(item => {
+            if (item.id.slice(4) == +page_number_value) {
+                item.classList.add('active')
+            }
+        })
+
+    }
+
+}
+)
+
+backButton.addEventListener('click', () => {
+
+    elemsOnPage_value = +page_selector.value;
+
+    if (page_number_value > 1) {
+
+        page_buttons_selector.forEach(item =>
+            item.classList.remove('active'))
+        --page_number_value;
+        renderElements(elemsOnPage_value, page_number_value)
+        page_buttons_selector.forEach(item => {
+            if (item.id.slice(4) == +page_number_value) {
+                item.classList.add('active')
+            }
+        })
+    }
+
+}
+)
 
 
 // like method
@@ -147,51 +228,41 @@ likeButton.forEach(item => {
 
     })
 })
-firstRender();
-//first render pages block
-updatePageCounter(my_data.positions.length, page_selector.value);
 
+jumpButton.addEventListener('click', (e) => {
+    let jumpAlertSpan = document.querySelector('.jump_Alert')
+    let pages_count= Math.ceil(my_data.positions.length / elemsOnPage_value);
+    if(jumpInput.value>0 && jumpInput.value<=pages_count ){
+    page_buttons_selector.forEach(item => {
+        console.log(item.id.slice(4),page_number_value)
+        if (item.id.slice(4)==+page_number_value)
+        item.classList.remove('active')
 
+})
 
-
-
-
-
-
-//add active class each child + render elements on pageNumber change
-
-nextButton.addEventListener('click', () => {
-
-    let elemsOnPage_value = +page_selector.value;
-
-
-    if (page_number_value < page_buttons_selector.length) {
-        page_buttons_selector.forEach(item =>
-            item.classList.remove('active'))
-
-        ++page_number_value;
-
-        renderElements(elemsOnPage_value, page_number_value)
-
-        page_buttons_selector[page_number_value - 1].classList.add('active');
+    let newPage = +jumpInput.value;
+renderElements(elemsOnPage_value, newPage)
+updateCurrentPage(newPage);
+page_buttons_selector.forEach(item => {
+    if (item.id.slice(4) == newPage) {
+        item.classList.add('active')
     }
-
+        jumpInput.value = '';
+})
+page_number_value=newPage;
+}
+else{
+    jumpAlertSpan.style.opacity='1';
+    jumpAlertSpan.innerHTML='Bad value';
+    setTimeout(() =>{
+        jumpAlertSpan.style.opacity='0'
+        jumpInput.value = '';
+    }, 1500)
+}
 }
 )
-
-backButton.addEventListener('click', () => {
-
-    let elemsOnPage_value = +page_selector.value;
-
-    if (page_number_value > 1) {
-
-        page_buttons_selector.forEach(item =>
-            item.classList.remove('active'))
-        --page_number_value;
-        renderElements(elemsOnPage_value, page_number_value)
-        page_buttons_selector[page_number_value - 1].classList.add('active');
-    }
-
+let checkValue = (object) => {
+    let pages_count= Math.ceil(my_data.positions.length / elemsOnPage_value);
+    if (object.value>pages_count)
+        object.value=pages_count;
 }
-)
-
